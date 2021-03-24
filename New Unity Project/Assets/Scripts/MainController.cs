ffc2c228366c1cb3931cd7983b94de88db92fa94
Dspsyuodd <varlamovartem1111@gmail.com>
2021-03-24 16:25:39 +0400

    123

---
+++ New Unity Project/Assets/Scripts/MainController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+    [SerializeField] private GameObject obst;
+
+    private float timer = 1.5f;
+
+    void Update()
+    {
+        if (timer < 0f)
+        {
+            timer = 1.5f;
+            GameObject a = Instantiate(obst, new Vector3(9.1f, Random.Range(0.1f, 4f), 0f), Quaternion.identity);
+            Destroy(a, 5);
+        }
+        timer -= Time.deltaTime;
+    }
+}
---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private bool isJump = false;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space))
+            isJump = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (isJump)
+        {
+            rb.velocity = new Vector2(0f, 0f);
+            rb.AddForce(new Vector2(0f, 500f));
+            isJump = false;
+        }
+    }
+}
